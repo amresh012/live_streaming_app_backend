c79816100f1f48e9639b29e3863a9cfcdba91a49
amresh012 <amreshmishra67099@gmail.com>
2025-04-29 02:51:50 +0530

    feat: Add morgan for logging and update dependencies in package.json and package-lock.json

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import http from 'http';
 import { Server as SocketIOServer } from 'socket.io';
 import NodeMediaServer from 'node-media-server';
 import cors from 'cors';
-
+import morgan from "morgan";
 import connectDB from './config/db.config.js';
 import nmsConfig from './nms.js';
 import authRoutes from './routes/auth.routes.js';
@@ -22,6 +22,7 @@ const io = new SocketIOServer(server, { cors: { origin: '*' } });
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 // Routes
 app.use('/api/auth', authRoutes);
